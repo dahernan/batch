06dcd4cb0b60835050b2a5d821a1884346c89380
David Hernandez <dahernan@gmail.com>
2016-10-04 17:44:20 +0900

    refactor to be a function

--- batch_test.go
+++ batch_test.go
@@ -12,66 +12,51 @@ type Item struct {
 	field string
 }
 
-type Batcher struct {
-	ctx  context.Context
-	doFn func(items []Item)
+// type Batcher struct {
+// 	ctx  context.Context
+// 	doFn func(items []Item)
+//
+// 	buffer    []Item
+// 	batchSize int
+// 	sendCh    chan Item
+// }
 
-	buffer    []Item
-	batchSize int
-	sendCh    chan Item
-}
+func Batcher(ctx context.Context, batchSize int, doFn func(items []Item)) chan Item {
+	buffer := make([]Item, 0, batchSize)
+	sendCh := make(chan Item)
 
-func NewBatcher(ctx context.Context, batchSize int, doFn func(items []Item)) *Batcher {
-	b := Batcher{
-		ctx:       ctx,
-		doFn:      doFn,
-		batchSize: batchSize,
-		buffer:    make([]Item, 0, batchSize),
-		sendCh:    make(chan Item),
-	}
-
-	go b.run()
-	return &b
-}
-
-func (b *Batcher) Send(item Item) {
-	b.sendCh <- item
-}
-
-func (b *Batcher) run() {
-	for {
-		select {
-		case item := <-b.sendCh:
-			b.buffer = append(b.buffer, item)
-			if len(b.buffer) >= b.batchSize {
-				b.do()
+	go func() {
+		for {
+			select {
+			case item := <-sendCh:
+				buffer = append(buffer, item)
+				if len(buffer) >= batchSize {
+					doFn(buffer)
+					buffer = make([]Item, 0, batchSize)
+				}
+			case <-ctx.Done():
+				doFn(buffer)
+				return
 			}
-		case <-b.ctx.Done():
-			b.do()
-			return
 		}
-	}
-}
-
-func (b *Batcher) do() {
-	b.doFn(b.buffer)
-	b.buffer = make([]Item, 0, b.batchSize)
+	}()
+	return sendCh
 }
 
 func TestBatch(t *testing.T) {
 	is := is.New(t)
 
-	b := NewBatcher(context.Background(), 3, func(items []Item) {
+	b := Batcher(context.Background(), 3, func(items []Item) {
 		fmt.Printf("Some batch %+v\n", items)
 	})
 
-	b.Send(Item{"one"})
-	b.Send(Item{"two"})
-	b.Send(Item{"three"})
+	b <- Item{"one"}
+	b <- Item{"two"}
+	b <- Item{"three"}
 
-	b.Send(Item{"four"})
-	b.Send(Item{"five"})
-	b.Send(Item{"six"})
+	b <- Item{"four"}
+	b <- Item{"five"}
+	b <- Item{"six"}
 
 	is.True(false)
 }
