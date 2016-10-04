10f7d01deefae3d6374bdf19fe4ae17ed845b433
David Hernandez <dahernan@gmail.com>
2016-10-04 18:23:38 +0900

    flush interval

--- batch_test.go
+++ batch_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/cheekybits/is"
 )
@@ -12,9 +13,22 @@ type Item struct {
 	field string
 }
 
-func Batcher(ctx context.Context, batchSize int, doFn func(ctx context.Context, items []interface{})) chan interface{} {
-	buffer := make([]interface{}, 0, batchSize)
-	sendCh := make(chan interface{})
+func Batcher(ctx context.Context, batchSize int, flushInterval time.Duration, doFn func(ctx context.Context, items []interface{})) chan interface{} {
+	var (
+		tickCh    <-chan time.Time
+		ticker    *time.Ticker
+		lastBatch time.Time
+
+		buffer []interface{}
+		sendCh chan interface{}
+	)
+	buffer = make([]interface{}, 0, batchSize)
+	sendCh = make(chan interface{})
+
+	if flushInterval != 0 {
+		ticker = time.NewTicker(flushInterval)
+		tickCh = ticker.C
+	}
 
 	go func() {
 		for {
@@ -23,11 +37,22 @@ func Batcher(ctx context.Context, batchSize int, doFn func(ctx context.Context, 
 				buffer = append(buffer, item)
 				if len(buffer) >= batchSize {
 					doFn(ctx, buffer)
+					lastBatch = time.Now()
+					buffer = make([]interface{}, 0, batchSize)
+				}
+			case <-tickCh:
+				nextBatch := lastBatch.Add(flushInterval)
+				now := time.Now()
+				if len(buffer) > 0 && now.After(nextBatch) {
+					doFn(ctx, buffer)
+					lastBatch = now
 					buffer = make([]interface{}, 0, batchSize)
 				}
 			case <-ctx.Done():
-				doFn(ctx, buffer)
-				buffer = nil
+				if len(buffer) > 0 {
+					doFn(ctx, buffer)
+					buffer = nil
+				}
 				return
 			}
 		}
@@ -39,12 +64,33 @@ func TestBatch(t *testing.T) {
 	is := is.New(t)
 
 	ctx := context.Background()
-	b := Batcher(ctx, 3, func(ctx context.Context, items []interface{}) {
+	b := Batcher(ctx, 3, 0, func(ctx context.Context, items []interface{}) {
 		fmt.Printf("Some batch %+v\n", items)
 	})
 
 	b <- Item{"one"}
 	b <- Item{"two"}
+	b <- Item{"three"}
+
+	b <- Item{"four"}
+	b <- Item{"five"}
+	b <- Item{"six"}
+
+	is.True(false)
+}
+
+func TestBatchFlush(t *testing.T) {
+	is := is.New(t)
+
+	ctx := context.Background()
+	b := Batcher(ctx, 3, 2*time.Second, func(ctx context.Context, items []interface{}) {
+		fmt.Printf("Some batch flush %+v\n", items)
+	})
+
+	b <- Item{"one"}
+	b <- Item{"two"}
+
+	time.Sleep(3 * time.Second)
 	b <- Item{"three"}
 
 	b <- Item{"four"}
